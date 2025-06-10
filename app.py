6c438f0f74b247e3fbabce1ed498f10c9ab49d56
kudiyarohit <2022csrohit12759@poornima.edu.in>
2025-06-10 17:29:45 +0530

    Updates

--- app.py
+++ app.py
@@ -1,56 +1,109 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SECRET_KEY'] = 'your-secret-key'
 
 db = SQLAlchemy(app)
+
+# Flask-Login setup
+login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+# Models
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    todos = db.relationship('Todo', backref='user', lazy=True)
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
-    
-    def __repr__(self) -> str:
-        return f"{self.sno} - {self.title}"
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-@app.route("/", methods = ['GET', 'POST'])
+# Login loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Routes
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if user already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists. Please choose another one.', 'danger')
+            return redirect('/register')
+
+        # Create and add new user
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful. Please login.', 'success')
+        return redirect('/login')
+
+    return render_template('register.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect("/")
+        flash("Invalid credentials", "danger")
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+@app.route("/", methods=['GET', 'POST'])
+@login_required
 def home():
     if request.method == 'POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo(title = title, desc=desc)
-        db.session.add(todo)
-        db.session.commit()
-    allTodo = Todo.query.all()
-    return render_template("index.html", allTodo = allTodo)
-
-@app.route("/show")
-def show():
-    allTodo = Todo.query.all()
-    return f"{allTodo}"
-
-@app.route("/update/<int:sno>", methods = ['GET', 'POST'])
-def update(sno):
-    if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
-        todo = Todo.query.filter_by(sno=sno).first()
-        todo.title = title
-        todo.desc = desc
+        todo = Todo(title=title, desc=desc, user_id=current_user.id)
         db.session.add(todo)
         db.session.commit()
         return redirect("/")
-    
-    todo = Todo.query.filter_by(sno=sno).first()
-    return render_template("update.html", todo = todo)
+    allTodo = Todo.query.filter_by(user_id=current_user.id).all()
+    return render_template("index.html", allTodo=allTodo)
+
+@app.route("/update/<int:sno>", methods=['GET', 'POST'])
+@login_required
+def update(sno):
+    todo = Todo.query.filter_by(sno=sno, user_id=current_user.id).first_or_404()
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        db.session.commit()
+        return redirect("/")
+    return render_template("update.html", todo=todo)
 
 @app.route("/delete/<int:sno>")
+@login_required
 def delete(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
+    todo = Todo.query.filter_by(sno=sno, user_id=current_user.id).first_or_404()
     db.session.delete(todo)
     db.session.commit()
     return redirect("/")
